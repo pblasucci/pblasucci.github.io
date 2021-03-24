4e69d048f8cfc373ecdcb828f46092bc9adff01a
Paul Blasucci <pblasucci@gmail.com>
2021-03-24 23:13:50 +0100

    complete workflow implemented

--- Move-Content.ps1
+++ Move-Content.ps1
@@ -12,7 +12,7 @@
     .PARAMETER Stage
         Specifies which step of the workflow should be executed; valid values are:
         1. Render -- this will convert drafts into a form suitable for final review/tweaks
-        2. Publish -- updates timestamps and the final listing (index)
+        2. Publish -- updates timestamps and the final listings (index, tag sets)
 
     .EXAMPLE
         PS C:\> .\Move-Content.ps1 -Stage Render
@@ -25,27 +25,238 @@
 param(
     [Parameter(Mandatory=$True)]
     [ValidateSet('Render', 'Publish')]
-    [string] $Stage
+    [string] $Stage,
+
+    [Parameter(Mandatory=$False)]
+    [string[]] $Include,
+
+    [Parameter(Mandatory=$False)]
+    [string[]] $Exclude,
+
+    [Parameter(Mandatory=$False)]
+    [Switch] $Preserve
 )
 
+#
+# Utility which helps bind key-value data into textual content
+function Invoke-Template {
+    param([ScriptBlock] $scriptBlock)
+
+    function Render-Template {
+        param([string] $template)
+
+        Invoke-Expression "@`"`r`n$template`r`n`"@"
+    }
+
+    & $scriptBlock
+}
+
+
+#
+# Transitions production-ready content into a final stage
 function Publish {
-    #   build list from contents of docs/index.html
-    #   for each .html in ready/
-    #       update publication timestamp in body
-    #       generate index entry
-    #       append index entry to list from step 1
-    #       copy updated file to docs/
-    Write-Debug 'Publish'
+    # output location for individual articles
+    $postsFolder = ./docs/posts
+    if (-Not (Test-Path -Path $postsFolder)) {
+        New-Item -ItemType Directory $postsFolder | Write-Verbose
+    }
+
+    # output location for article listings
+    $listsFolder = ./docs/lists
+    if (-Not (Test-Path -Path $listsFolder)) {
+        New-Item -ItemType Directory $listsFolder | Write-Verbose
+    }
+
+    #
+    # loop over all "ready" articles (optionally including/excluding some); for each:
+    #   + update publication
+    #   + synchronize tag metadata between HEAD and BODY
+    #   + render updated HTML
+    #   + emit metadata (to be used by the listings)
+    #   + (optionally) remove "ready" artifact
+    $postMeta = Get-ChildItem ./ready/posts -Filter *.html -Include $Include -Exclude $Exclude | ForEach-Object {
+        $fullPath = $_.FullName
+        $baseName = $_.BaseName
+
+        # load ready layout
+        $postTemplate = Get-Content $fullPath -Raw | Out-String
+
+        Invoke-Template {
+            # update publication metadata
+            $publishedAt = [DateTimeOffset]::Now
+            $pubISO_8601 = $publishedAt.ToString("o")
+            $pubFriendly = $publishedAt.ToString("dddd, dd MMMM yyyy, 'at' HH:mm 'GMT' K")
+
+            # extract post title
+            if ($postTemplate -match '(?<=id="post_title">)[^<]+') {
+                $postName = $Matches[0].Trim()
+            } else {
+                throw "Could not determine post title! (file: $fullPath)"
+            }
+
+            # process tag metadata
+            if ($postTemplate -match '(?<=name="tags"\s*content=")[^"\n]*') {
+                $tagList = $Matches[0].Split(';').Trim()
+                $postTags = ($tagList | ForEach-Object {
+                    "<li><a href=`"../lists/$([uri]::EscapeDataString($_)).html`">$_</a></li>"
+                }) -join [Environment]::NewLine
+            } else {
+                $postTags = [string]::Empty
+            }
+
+            # emit final content
+            $postFile = Join-Path -Path $postsFolder -ChildPath "$($baseName).html"
+            if (Test-Path -Path $postFile) {
+                Remove-Item $postFile
+            }
+            Render-Template $postTemplate | Out-File -Path $postFile
+
+            # pass metadata downstream
+            $meta = @{
+                PostFile = "../posts/$($baseName).html"
+                PostName = $postName
+                PostTags = $postTags
+                TagList = $tagList
+                PubISO_8601 = $pubISO_8601
+                PubFriendly = $pubFriendly
+            }
+
+            Write-Output $meta
+        }
+
+        # clean up
+        if (-Not $Preserve) { Remove-Item $fullPath -Force }
+    }
+
+    #
+    # LISTINGS
+    # ======================================================================
+
+    # template for an individual article in a listing
+    $itemTemplate = '<li class="entry">
+        <a href="$postFile">$postName</a>
+        <span>
+        Published:
+        <time datetime="$pubISO_8601">$pubFriendly</time>
+        </span>
+        <ul class="tags">
+            $postTags
+        </ul>
+    </li>'
+
+    # load listing layout
+    $listTemplate = Get-Content ./look/list.html -Raw | Out-String
+
+    # project article metadata into a mapping from tag to a collectin of article metadata
+    $tagLists = @{}
+    $postMeta | ForEach-Object {
+        $original = $_
+        $_.TagList | ForEach-Object {
+            $tagLists[$_] += @($original)
+        }
+    }
+
+    #
+    # loop over all metadata tags; for each:
+    #   + generate a listing of all articles with current tag
+    #   + render listing into layout
+    $tagLists.Keys | ForEach-Object {
+        $listName = $_
+        $listFile = Join-Path -Path $listsFolder -ChildPath "$($listName).html"
+
+        # render tag index page
+        Invoke-Template {
+
+            # build fragment of list entries for associated articles
+            $listItems = ($tagLists[$listName] | ForEach-Object {
+                $postFile = $_.PostFile
+                $postName = $_.PostName
+                $postTags = $_.PostTags
+                $pubISO_8601 = $_.PubISO_8601
+                $pubFriendly = $_.PubFriendly
+
+                Render-Template $itemTemplate
+            }) -join [Environment]::NewLine
+
+            # emit final content
+            if (Test-Path -Path $listFile) {
+                Remove-Item $listFile -Force
+            }
+            Render-Template $listTemplate | Out-File -Path $listFile
+        }
+    }
+
+    $indexFile = './docs/index.html'
+    $indexTemplate = Get-Content ./look/root.html -Raw | Out-String
+
+    # render root index page
+    Invoke-Template {
+
+        # build fragment of list entries for associated articles, adjusting metadata (pathing) as needed
+        $listItems = ($postMeta | ForEach-Object {
+            $postFile = ($_.PostFile -replace '\.\./', './')
+            $postName = $_.PostName
+            $postTags = ($_.PostTags -replace '\.\./', './')
+            $pubISO_8601 = $_.PubISO_8601
+            $pubFriendly = $_.PubFriendly
+
+            Render-Template $itemTemplate
+        }) -join [Environment]::NewLine
+
+        # emit final content
+        if (Test-Path -Path $indexFile) {
+            Remove-Item $indexFile -Force
+        }
+        Render-Template $indexTemplate | Out-File -Path $indexFile
+    }
 }
 
+
+#
+# Transitions draft content into a production review stage
 function Render {
-    #   for each .md in draft/
-    #       generate HTML content from markdown
-    #       generate new file from template + previous step
-    #       wrote previous step to ready/
-    Write-Debug 'Render'
+    # output location for "ready" articles
+    $postsFolder = './ready/posts/'
+    if (-Not (Test-Path -Path $postsFolder)) {
+        New-Item -ItemType Directory $postsFolder | Write-Verbose
+    }
+
+    # load article layout
+    $template = Get-Content ./look/post.html -Raw | Out-String
+
+    #
+    # loop over all drafts (optionally including/excluding some); for each:
+    #   + render (most) HTML
+    #   + seed template tokens for next stage
+    #   + (optionally) remove draft
+    Get-ChildItem ./draft -Filter *.md -Include $Include -Exclude $Exclude | ForEach-Object {
+        $fullPath = $_.FullName
+        $baseName = $_.BaseName
+
+        Invoke-Template {
+            # preserve variables needed for next stage
+            $pubISO_8601 = '$pubISO_8601'
+            $pubFriendly = '$pubFriendly'
+            $postTags = '$postTags'
+
+            # process content
+            $markdown = Get-Content $fullPath -Raw | ConvertFrom-Markdown
+            $postFile = Join-Path -Path $postsFolder -ChildPath "$($baseName).html"
+            $postHtml = $markdown | Select-Object -ExpandProperty Html
+
+            # render output
+            Render-Template $template | Out-File -Path $postFile
+        }
+
+        # clean up
+        if (-Not $Preserve) { Remove-Item $fullPath -Force }
+    }
 }
 
+
+#
+# ENTRY POINT
+# ======================================================================
 switch ($Stage) {
     'Render'  { Render  }
     'Publish' { Publish }
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 pblasucci.github.io
 ===
 
-Paul Blasucci's Weblog
+This repository contains the content and support files for my personal weblog. I follow a simple three-stage writing
+workflow (described here, _mostly_, so I don't forget the details). The three stages are as follows:
+
+1.  _Draft_... Heavy on creative flow; light on structure/editing/proofing/presentation/etc.
+1.  _Ready_... Thoughts crystallized; focus is on editing, proofing, and presentation.
+1.  _Final_... Content is published; no changes (barring editorial corrections).
+
+Each stage of the workflow has an associated folder in the repository. These folders will contain artifacts as follows:
+
+Stage   | Folder    | Content
+--------|-----------|--------------------------
+Draft   | `./draft` | Markdown files
+Ready   | `./ready`  | A mix of HTML files and media (JPG, PNG, SVG, etc.)
+Final   | `./docs`   | A mix of HTML files, media (JPG, PNG, SVG, etc.), and styling (CSS)
+
+Finally, a small Powershell script, `Move-Content.ps1`, is provider to facilitate moving between stages.
+
+`TODO document high-level usage of Move-Content.ps1`
